e9a6dde614652ef9b1d4021d39a2a560a138c064
Rogerio F Guimaraes <koridallos@Rogerio-MacOfficial.local>
2019-08-13 17:15:15 -0300

    app yaml

--- server.js
+++ server.js
@@ -9,7 +9,8 @@ app.get('/',function(request,response){
 	response.sendFile(__dirname + '/index.html');
 })
 
+const PORT = process.env.PORT || 80;
 
-const listener = app.listen(80, function() {
+const listener = app.listen(PORT, function() {
 	console.log('Aplicação disponível na porta 5001' );
 });
